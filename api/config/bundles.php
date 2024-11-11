2a7310f46ef3ec7dfb482d6a378a30dd5342fff0
mathieu_cstn <mathieuconstantin@gmail.com>
2024-11-11 23:45:53 +0100

    [S][+] Add Foundry and Doctrine Fixtures Bundle
    - Add Foundry dependency
    - Add Doctrine Fixtures Bundle dependency

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
