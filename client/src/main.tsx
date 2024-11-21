2ca872068b745a79fcc4922e75628f39e3d6f0d6
mathieu_cstn <mathieuconstantin@gmail.com>
2024-11-21 17:02:59 +0100

    [C][+] Add typesafe for Tanstack Router
    - Config src/main.tsx
    - Add TanStackRouterVite to Vite plugins

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,12 @@ import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
 
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
