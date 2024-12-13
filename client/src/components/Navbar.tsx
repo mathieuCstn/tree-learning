4b3c7a6b4d6e6819f57169fc79b14d892e6907f0
mathieu_cstn <mathieuconstantin@gmail.com>
2024-12-14 00:53:43 +0100

    [C][~] Factorize the navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,20 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
+const links = [
+  {to: "/dashboard", title: "Dashboard"},
+  {to: "/quizzes", title: "Quizzes"},
+  {to: "/questions", title: "Questions"},
+  {to: "/users", title: "Users list"},
+  {to: "/assessment-sessions", title: "My quizzes"}
+];
+
 const Navbar = (): JSX.Element => {
   const { user, logoutUser } = useAuth();
 
   return (
     <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
-      <Link to="/dashboard" style={{ marginRight: "1rem" }}>
-        Dashboard
-      </Link>
-      <Link to="/quizzes" style={{ marginRight: "1rem" }}>
-        Quizzes
-      </Link>
-      <Link to="/questions" style={{ marginRight: "1rem" }}>
-        Questions
-      </Link>
+      {links.map(({to, title}, index) => (
+        <Link key={index} to={to} style={{ marginRight: "1rem" }}>
+          {title}
+        </Link>
+      ))}
       {user ? (
         <button onClick={logoutUser} style={{ marginLeft: "auto" }}>
           Logout
