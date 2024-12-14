c7eed0623b8377f60aef8fdc24cbe8fd14880ca3
mathieu_cstn <mathieuconstantin@gmail.com>
2024-12-14 01:21:16 +0100

    [C][+] Handle private routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,10 @@ import QuizzesPage from "./pages/QuizzesPage";
 import PrivateRoute from "./components/PrivateRoute";
 import QuizDetailPage from "./pages/QuizDetailPage";
 import QuestionsPage from "./pages/QuestionsPage";
+import UsersPage from "./pages/UsersPage";
+import UserDetailPage from "./pages/UserDetailPage";
+import AssessmentSessionsPage from "./pages/AssessmentSessionsPage";
+import QuizPage from "./pages/QuizPage";
 
 const App = (): JSX.Element => {
   return (
@@ -20,6 +24,10 @@ const App = (): JSX.Element => {
           <Route path="/quizzes" element={<PrivateRoute><QuizzesPage /></PrivateRoute>} />
           <Route path="/quizzes/:id" element={<PrivateRoute><QuizDetailPage /></PrivateRoute>} />
           <Route path="/questions" element={<PrivateRoute><QuestionsPage /></PrivateRoute>} />
+          <Route path="/users" element={<PrivateRoute roles={["ROLE_ADMIN"]}><UsersPage /></PrivateRoute>} />
+          <Route path="/users/:id" element={<PrivateRoute><UserDetailPage /></PrivateRoute>} />
+          <Route path="/assessment-sessions" element={<PrivateRoute><AssessmentSessionsPage /></PrivateRoute>} />
+          <Route path="/quiz/:quizId" element={<PrivateRoute><QuizPage /></PrivateRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -4,12 +4,24 @@ import { useAuth } from "../context/AuthContext";
 
 interface PrivateRouteProps {
   children: JSX.Element;
+  roles?: string[]; // Liste des rôles autorisés
 }
 
-const PrivateRoute = ({ children }: PrivateRouteProps): JSX.Element => {
+const PrivateRoute = ({ children, roles }: PrivateRouteProps): JSX.Element => {
   const { user } = useAuth();
 
-  return user ? children : <Navigate to="/login" />;
+  // Si l'utilisateur n'est pas connecté
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  // Vérifie si l'utilisateur a au moins un rôle autorisé
+  if (roles && !roles.some((role) => user.roles.includes(role))) {
+    return("Vous n'êtes pas autorisé à voir cette page");
+  }
+
+  // Autoriser l'accès si aucune restriction ou rôle correct
+  return children;
 };
 
 export default PrivateRoute;
